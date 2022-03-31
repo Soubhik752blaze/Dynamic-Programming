8e50b5ec8934587c9bfe8192d0d9e2d44749ca81
Ghosh Soubhik <soubhik.ghosh@nokia.com>
2022-03-31 12:38:19 +0530

    count subset sum problem

---
+++ countsubsetsum.cpp
@@ -0,0 +1,11 @@
+/*Question :- Count of subsets sum with a Given sum
+Given an array arr[] of length N and an integer X, the task is to find the number of subsets with sum equal to X.*/
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    system("cls");
+    cout<<"Hello";
+    return 0;
+}
