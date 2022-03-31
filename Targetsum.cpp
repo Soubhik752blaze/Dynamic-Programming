532198cfc2a42776b45704ff352e204a410d18cb
Ghosh Soubhik <soubhik.ghosh@nokia.com>
2022-03-31 12:37:33 +0530

    Target Sum problem

---
+++ Targetsum.cpp
@@ -0,0 +1,14 @@
+/*Question :- Target Sum Problem
+Given a list of non-negative integers, a1, a2, ..., an, and a target, S. Now you have 2 symbols + and -. 
+For each integer, you should choose one from + and - as its new symbol.
+Find out how many ways to assign symbols to make sum of integers equal to target S.*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    system("cls");
+    cout<<"Hello";
+    return 0;
+}
