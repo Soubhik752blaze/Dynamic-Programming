efef6b8cfa3f38c2e3649c666b9830cd4debadcc
Soubhik <65910085+Soubhik752blaze@users.noreply.github.com>
2022-04-12 18:55:30 +0530

    Update Targetsum.cpp

--- Targetsum.cpp
+++ Targetsum.cpp
@@ -6,9 +6,55 @@ Find out how many ways to assign symbols to make sum of integers equal to target
 #include<bits/stdc++.h>
 using namespace std;
 
-int main()
+int findTargetSumWays(vector<int>&A ,int target)
+    {
+        int S=target;;
+        int sum = 0;
+        int n =A.size();
+        for(int i =0;i<n;i++)
+            sum += A[i];
+        if(S>sum || (sum-S)%2!=0)
+            return 0;
+        
+        int dp[n+1][((sum-S)/2)+1];
+        
+        int c=0;
+        for(int i=0;i<n;i++)
+            if(A[i]==0)
+                c++;
+        
+        for(int i=0;i<n+1;i++)
+            dp[i][0]=1;    
+            
+        for(int i=1;i<((sum-S)/2)+1;i++)
+            dp[0][i] = 0;
+        
+        for(int i=1;i<n+1;i++)
+            for(int j=1;j<((sum-S)/2)+1;j++)
+            {
+                if(A[i-1]==0)
+                    dp[i][j] = dp[i-1][j];
+                else
+                if(A[i-1]<=j)
+                    dp[i][j] = dp[i-1][j-A[i-1]] + dp[i-1][j];
+                else
+                    dp[i][j] = dp[i-1][j];
+            }
+        
+        
+        return pow(2,c)*dp[n][(sum-S)/2];
+        }
+};
+
+
+
+int main() 
 {
-    system("cls");
-    cout<<"Hello";
-    return 0;
-}
+    int target = 1,n=3;
+    vector<int> A(n);
+    for(int i=0;i<n;i++)
+        cin>>A[i];
+    cout<<"No of ways: "<<findTargetSumWays(A,target);
+    }
+     
+      
