383bcc5a57f1fd5f8e38a9a882f3cbbf8228163b
Soubhik <65910085+Soubhik752blaze@users.noreply.github.com>
2022-04-12 19:12:42 +0530

    Update Targetsum.cpp

--- Targetsum.cpp
+++ Targetsum.cpp
@@ -1,14 +1,17 @@
 /*Question :- Target Sum Problem
 Given a list of non-negative integers, a1, a2, ..., an, and a target, S. Now you have 2 symbols + and -. 
 For each integer, you should choose one from + and - as its new symbol.
-Find out how many ways to assign symbols to make sum of integers equal to target S.*/
+Find out how many ways to assign symbols to make sum of integers equal to target S.
+
+
+                    THIS QUESTION IS SAME AS "COUNT NO OF SUBSETS WITH A GIVEN DIFFERENCE OF SUBSETSUM"       */
 
 #include<bits/stdc++.h>
 using namespace std;
 
 int findTargetSumWays(vector<int>&A ,int target)
     {
-        int S=target;;
+        int S=target;
         int sum = 0;
         int n =A.size();
         for(int i =0;i<n;i++)
