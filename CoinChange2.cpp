453a706c7258beae6125a5ccf8093712084311b8
Soubhik <soubhik285@gmail.com>
2022-10-19 02:18:14 +0530

    Comments fixed

---
+++ CoinChange2.cpp
@@ -0,0 +1,23 @@
+/*Coin Change Problem Minimum Numbers of coins
+We are given an array Arr with N distinct coins and a target. 
+We have an infinite supply of each coin denomination. 
+We need to find the number of ways we sum up the coin values to give us the target.
+
+Each coin can be used any number of times.*/
+#include<bits/stdc++.h>
+using namespace std;
+
+int recursive(){
+    
+}
+
+int
+int main()
+{
+  vector<int> arr ={1,2,3};
+  int target=4;
+  
+  int n =arr.size();
+                                 
+  cout<<"The total number of ways is " <<countWaysToMakeChange(arr,n,target);
+}
