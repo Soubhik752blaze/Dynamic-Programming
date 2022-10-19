b1ff68da41202422cedf3dc7d8a84c769b7d4aa2
Soubhik <soubhik285@gmail.com>
2022-10-19 20:52:05 +0530

    Code fixed and comments added

--- CoinChange2.cpp
+++ CoinChange2.cpp
@@ -7,17 +7,112 @@ Each coin can be used any number of times.*/
 #include<bits/stdc++.h>
 using namespace std;
 
-int recursive(){
+// same as minimum coins problem
+// only difference here is we need to count the no of ways we can arrange coins
+// so accordingly we change our base case to return 1 for every valid way and 0 for not valid ways
+// for return statement, we add the ways instead of taking minimum
+int recursive(int i, int target, vector<int> &arr){
+    //base case
+    //target == 0 use case not included as traversing more elements might give more results
+    if (i == 0)
+        {
+            if (target % arr[0] == 0)
+                return 1;
+            else
+                return 0;
+        }
     
+    // explore all possibilites
+    int not_take = recursive(i - 1, target, arr);
+    int take = 0;
+    if (arr[i] <= target)
+        take = recursive(i, target - arr[i], arr);
+    // return all ways
+    return take + not_take;
+}
+int memoization(int i, int target, vector<int> &arr, vector<vector<int>> &dp)
+    {
+        // basecase
+        if (i == 0)
+        {
+            if (target % arr[0] == 0)
+                return 1;
+            else
+                return 0;
+        }
+        if(dp[i][target] != -1)
+            return dp[i][target];
+        // explore all possibilites
+        int not_take = memoization(i - 1, target, arr, dp);
+        int take = 0;
+        if (arr[i] <= target)
+            take = memoization(i, target - arr[i], arr, dp);
+        // return all ways
+        return dp[i][target] = take + not_take;
+    }
+int tabulation(int k, vector<int> arr)
+{
+    int n = arr.size();
+    vector<vector<int>> dp(n, vector<int>(k + 1, 0));
+    // base case creation for tabulation
+    for (int i = 0; i <= k; i++)
+    {
+        if (i % arr[0] == 0)
+            dp[0][i] = 1;
+        else
+            dp[0][i] = 0;
+    }
+    //for loop and same recurrence 
+    for (int i = 1; i < n; i++)
+        for (int target = 0; target <= k; target++)
+        {
+            int not_taken = dp[i - 1][target];
+            int taken = 0;
+            if (arr[i] <= target)
+                taken = 1 + dp[i][target - arr[i]];
+            dp[i][target] = min(taken, not_taken);
+        }
+    //return statement
+    return dp[n - 1][k];
+}
+int spaceoptimised(int k, vector<int> arr)
+{
+    int n = arr.size();
+    vector<int> curr(k + 1, 0), prev(k+1, 0);
+    // base case creation for tabulation
+    for (int i = 0; i <= k; i++)
+    {
+        if (i % arr[0] == 0)
+            prev[i] = 1;
+        else
+            prev[i] = 0;
+    }
+    //for loop and same recurrence 
+    for (int i = 1; i < n; i++){
+        for (int target = 0; target <= k; target++)
+        {
+            int not_taken = prev[target];
+            int taken = 0;
+            if (arr[i] <= target)
+                taken = 1 + curr[target - arr[i]];
+            curr[target] = min(taken, not_taken);
+        }
+        prev = curr;
+    }
+    //return statement
+    return prev[k];
 }
 
-int
+int countWaysToMakeChange(int amount, vector<int> &coins)
+    {
+        int n = coins.size();
+        vector<vector<int>> dp(n, vector<int> (amount + 1, -1));
+        return memoization(n - 1, amount, coins, dp);
+    }
 int main()
 {
-  vector<int> arr ={1,2,3};
-  int target=4;
-  
-  int n =arr.size();
+  vector<int> arr ={1,2,5};
+  int target=5;
                                  
-  cout<<"The total number of ways is " <<countWaysToMakeChange(arr,n,target);
+  cout<<"The total number of ways is " <<countWaysToMakeChange(target, arr);
 }
--- mincoins.cpp
+++ mincoins.cpp
@@ -5,26 +5,126 @@ We can pick a coin denomination for any number of times we want.*/
 #include <bits/stdc++.h>
 using namespace std;
 
-int recursive()
+// same as subsetsum with target problem, we can pick or not pick a element
+// but here for every coin, supply is infinite
+// So, when we take a coin, we donot move the further down index and keep taking it until the target gets smaller than the coin value
+// Then, since we need min no of coins, we take min of take and not_Take solutions
+int recursive(int i, vector<int> &arr, int target)
 {
-    //base case
+    // base case
+    if (i == 0)
+    {
+        if (target % arr[0] == 0)
+            return target / arr[0];
+        else
+            return 1e9;
+    }
 
-
-    //explore all posibilites
-    
+    // explore all posibilites
+    int not_taken = recursive(i - 1, arr, target);
+    int taken = 1e9;
+    if (arr[i] <= target)
+    {
+        taken = 1 + recursive(i, arr, target - arr[i]);
+    }
+    // return minimum
+    return min(taken, not_taken);
 }
 
-int minimumElements(vector<int> &arr, int target)
+int memoization(int i, int target, vector<int> &arr, vector<vector<int>> &dp)
+{
+    // base case
+    if (i == 0)
+    {
+        if (target % arr[0] == 0)
+            return target / arr[0];
+        else
+            return 1e9;
+    }
+
+    if (dp[i][target] != -1)
+        return dp[i][target];
+
+    // explore all posibilites
+    int not_taken = memoization(i - 1, target, arr, dp);
+    int taken = 1e9;
+    if (arr[i] <= target)
+    {
+        taken = 1 + memoization(i, target - arr[i], arr, dp);
+    }
+    return min(taken, not_taken);
+}
+
+int tabulation(int k, vector<int> &arr)
 {
     int n = arr.size();
-    int ans = recursive(n - 1, arr, target);
+    vector<vector<int>> dp(n, vector<int>(k + 1, 0));
+    // base case creation for tabulation
+    for (int i = 0; i <= k; i++)
+    {
+        if (i % arr[0] == 0)
+            dp[0][i] = i / arr[0];
+        else
+            dp[0][i] = 1e9;
+    }
+    //for loop and same recurrence 
+    for (int i = 1; i < n; i++)
+        for (int target = 0; target <= k; target++)
+        {
+            int not_taken = dp[i - 1][target];
+            int taken = 1e9;
+            if (arr[i] <= target)
+                taken = 1 + dp[i][target - arr[i]];
+            dp[i][target] = min(taken, not_taken);
+        }
+    //return statement
+    return dp[n - 1][k];
+}
+
+
+int spaceoptimised(int k, vector<int> &arr)
+{
+    int n = arr.size();
+    vector<int> prev(k + 1, 0), curr(k+1, 0);
+    // base case creation for tabulation
+    for (int i = 0; i <= k; i++)
+    {
+        if (i % arr[0] == 0)
+            prev[i] = i / arr[0];
+        else
+            prev[i] = 1e9;
+    }
+    //for loop and same recurrence 
+    for (int i = 1; i < n; i++){
+        for (int target = 0; target <= k; target++)
+        {
+            int not_taken = prev[target];
+            int taken = 1e9;
+            if (arr[i] <= target)
+                taken = 1 + curr[target - arr[i]];
+            curr[target] = min(taken, not_taken);
+        }
+        prev = curr;
+    }
+    //return statement
+    return prev[k];
+}
+
+int minimumcoins(vector<int> &arr, int target)
+{
+    int n = arr.size();
+    // vector<vector<int>> dp(n, vector<int>(target + 1, -1));
+    // int ans = memoization(n - 1, target, arr, dp);
+    int ans = spaceoptimised(target, arr);
+    if (ans >= 1e9)
+        return -1;
     return ans;
 }
 int main()
 {
-    vector<int> arr = {1, 2, 3};
-    int target = 7;
+    vector<int> arr = {9, 7, 6, 1};
+    int target = 13;
 
-    cout << "The minimum number of coins required to form the target sum is " << minimumElements(arr target);
+    cout << "The minimum number of coins required to form the target sum is " << minimumcoins(arr, target);
     return 0;
 }
