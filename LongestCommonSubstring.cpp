4ef3dfd986755257365f4fb124272872463dcbb1
Soubhik <soubhik285@gmail.com>
2022-10-26 03:32:15 +0530

    Code updated

---
+++ LongestCommonSubstring.cpp
@@ -0,0 +1,79 @@
+/* Question -
+Given two sequences, print longest substring present in both of them.
+A substring is a sequence that appears in a string in a contagious order
+
+For example, “ab”,  “bc”, “abc”, “a”, "b", "c" .. etc are subsequences of “abc”. */
+#include <bits/stdc++.h>
+using namespace std;
+
+// tabulation code is similar as that of LCS(subsequence) so we directly use it..
+// Here the only difference is the substring is contagious in nature so in a non match case we directly return 0 and not of max of 2 choices as done in LCS.
+
+int tabulation(string s1, string s2)
+{
+    int n = s1.size();
+    int m = s2.size();
+    int ans = 0;
+    // since base case has condition < 0, thus we create a dp matrix of (n+1)* (m+1)
+    // and for index i and j, we take i-1 and j-1th index of strings s1 and s2
+    // so for i==0, since there is no string index -1 so for for i=0 we populate dp cells with 0.. same for j==0
+    vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+    // base case(i==0 cells and j==0 cells) is already thus taken care of
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= m; j++)
+        {
+            if (s1[i - 1] == s2[j - 1])
+                {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                    ans = max(ans, dp[i][j]);
+                }
+            else
+                dp[i][j] = 0;
+        }  
+    }
+
+    return ans;
+}
+
+int spaceoptimisation(string s1, string s2)
+{
+    int n = s1.size();
+    int m = s2.size();
+    int ans = 0;
+    vector<int> prev(m + 1, 0), curr(m + 1, 0);
+    for (int ind1 = 1; ind1 <= n; ind1++)
+    {
+        for (int ind2 = 1; ind2 <= m; ind2++)
+        {
+            if (s1[ind1 - 1] == s2[ind2 - 1])
+                {
+                    curr[ind2] = 1 + prev[ind2 - 1];
+                    ans = max(ans, curr[ind2]);
+                }
+            else
+                curr[ind2] = 0;
+        }
+        prev = curr;
+    }
+    return ans;
+}
+
+int lengthofLCS(string s1, string s2)
+{
+    int n = s1.length();
+    int m = s2.length();
+    int ans = tabulation(s1, s2);
+    return ans;
+}
+int main()
+{
+    string s1 = "cbadfe";
+    string s2 = "afcbde";
+
+    cout << "Length of Longest Common Substring is " << lengthofLCS(s1, s2);
+    return 0;
+}
+
+// TC -> O(N*M) + O(N+M)
+// SC -> O(N*M)
--- printLCS.cpp
+++ printLCS.cpp
@@ -1,5 +1,5 @@
 /* Question -
-Given two sequences, find the length of longest subsequence present in both of them.
+Given two sequences, print longest subsequence present in both of them.
 A subsequence is a sequence that appears in the same relative order, but not necessarily contiguous.
 
 For example, “abc”,  “abg”, “bdf”, “aeg”,  ‘”acefg”, .. etc are subsequences of “abcdefg”. */
@@ -7,26 +7,13 @@ For example, “abc”,  “abg”, “bdf”, “aeg”,  ‘”acefg”, .. et
 #include <bits/stdc++.h>
 using namespace std;
 
-// intuition is we have to generate all cases and check for LCA "max" length, but we need to have recursion do it for us.
-//  for LCS checking, we do a character wise check for every character from last to 0th index for both strings.
-//  choice 1:- if two characters match, lcs length = 1 + look for lcs length in both strings excluding those characters which we verified.
-//  choice 2:- if two characters dont match, we go one index down (for lookout) of 1 string and again carry on the process of checking character wise for both strings.
-//  in choice 2, we make 2 choices i.e for each string we reduce index for lookout and then try to match for LCS. whichever way, we find the largest length of LCS, we return it.
-int recursive(int i, int j, string s1, string s2)
-{
-    // base case
-    if (i < 0 || j < 0)
-        return 0;
-    // trying out all possibilites
-    // match case
-    if (s1[i] == s2[j])
-        return 1 + recursive(i - 1, j - 1, s1, s2);
-    // not match case
-    else
-        return max(recursive(i, j - 1, s1, s2), recursive(i - 1, j, s1, s2));
-}
-
-int tabulation(string s1, string s2)
+// we find the dp array and length of LCS
+// We use the DP array itself to find the LCS and then store it in a string
+// in dp array, we check from [n][m] to [0][0] direction
+// we check if s1[i] == s2[j] i.e same character check, if we yes we store it at last place of resultant string and reduce last place index by 1
+// then we i-1,j-1 position
+// incase the match condition is not satisfied we go dp[1-1][j] or dp[i][j-1] whichever is greater and then again check for same character
+string tabulation(string s1, string s2)
 {
     int n = s1.size();
     int m = s2.size();
@@ -47,35 +34,37 @@ int tabulation(string s1, string s2)
         }
     }
 
-    return dp[n][m];
-}
+    int length = dp[n][m];
+    string ans = "";
+    for (int ind = 0; ind < length; ind++)
+        ans += "$";
+    int ind = length - 1;
 
-int spaceoptimisation(string s1, string s2)
-{
-    int n = s1.size();
-    int m = s2.size();
-
-    vector<int> prev(m + 1, 0), curr(m + 1, 0);
-    for (int ind1 = 1; ind1 <= n; ind1++)
+    int i = n, j = m;
+    while(i>0 && j>0)
     {
-        for (int ind2 = 1; ind2 <= m; ind2++)
+        if(s1[i-1] == s2[j-1])
+            {
+                ans[ind] = s1[i-1];
+                ind--;
+                i--,j--;
+            }
+        else if(dp[i-1][j] > dp[i][j-1])
         {
-            if (s1[ind1 - 1] == s2[ind2 - 1])
-                curr[ind2] = 1 + prev[ind2 - 1];
-            else
-                curr[ind2] = max(prev[ind2], curr[ind2 - 1]);
+            i--;
         }
-        prev = curr;
+        else{
+            j--;
+        }
     }
-
-    return prev[m];
+    return ans;
 }
 
-int lengthlcs(string s1, string s2)
+string printLCS(string s1, string s2)
 {
     int n = s1.length();
-    int m = s1.length();
-    int ans = spaceoptimisation(s1, s2);
+    int m = s2.length();
+    string ans = tabulation(s1, s2);
     return ans;
 }
 int main()
@@ -83,6 +72,9 @@ int main()
     string s1 = "abcdef";
     string s2 = "afcbde";
 
-    cout << "Length of LCS is " << lengthlcs(s1, s2);
+    cout << "LCS is " << printLCS(s1, s2);
     return 0;
 }
+
+// TC -> O(N*M) + O(N+M)
+// SC -> O(N*M)
