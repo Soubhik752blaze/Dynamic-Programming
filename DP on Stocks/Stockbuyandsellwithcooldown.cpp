858c64e9b8636ab8c5346941c177eabd9f510a91
Soubhik <soubhik285@gmail.com>
2022-11-19 02:26:04 +0530

    code updated

--- DP on Stocks/Stockbuyandsellwithcooldown.cpp
+++ DP on Stocks/Stockbuyandsellwithcooldown.cpp
@@ -1,20 +1,19 @@
-/*Question:- You are given an integer array prices where prices[i] is the price of a given stock on the ith day.
+/*Question:- You are given an array prices where prices[i] is the price of a given stock on the ith day.
 
-On each day, you may decide to buy and/or sell the stock. You can only hold at most one share of the stock at any time.
-However, you can buy it then immediately sell it on the same day.
+Find the maximum profit you can achieve. You may complete as many transactions as you like (i.e., buy one and sell one share of the stock multiple times) with the following restrictions:
 
-Find and return the maximum profit you can achieve.*/
+After you sell your stock, you cannot buy stock on the next day (i.e., cooldown one day)..*/
 
 #include <bits/stdc++.h>
 using namespace std;
 
-// there are multiple ways of buy and sell, since we have to try multiple ways we will go for recursion
-// this function tells us upto ith day, whats the max profit possible to make
-// parameters - i(index), buy(variable to check if buying is possible or not) and price(input)
+// this is same as stockbuyandsell2, the only difference being we cant do any transaction 1 day after selling\
+// so after every sell, we now move the index by 2 and not by 1. since 1 more day counts for cooldown
+// Accordingly base case now becomes i>= n
 int recursive(int i, int buy, vector<int> price, int n)
 {
     // base case
-    if (i == n)
+    if (i >= n)
         return 0;
     // explore all possibilites
     int profit;
@@ -28,7 +27,7 @@ int recursive(int i, int buy, vector<int> price, int n)
     else
     {
         // we do sell or we dont sell
-        profit = max(price[i] + recursive(i + 1, 1, price, n), recursive(i + 1, 0, price, n));
+        profit = max(price[i] + recursive(i + 2, 1, price, n), recursive(i + 1, 0, price, n));
     }
     // return max profit
     return profit;
@@ -37,7 +36,7 @@ int recursive(int i, int buy, vector<int> price, int n)
 int memoization(int i, int buy, vector<int> price, int n, vector<vector<int>> &dp)
 {
     // base case
-    if (i == n)
+    if (i >= n)
         return 0;
 
     if (dp[i][buy] != -1)
@@ -54,7 +53,7 @@ int memoization(int i, int buy, vector<int> price, int n, vector<vector<int>> &d
     else
     {
         // we do sell or we dont sell
-        profit = max(price[i] + memoization(i + 1, 1, price, n, dp), memoization(i + 1, 0, price, n, dp));
+        profit = max(price[i] + memoization(i + 2, 1, price, n, dp), memoization(i + 1, 0, price, n, dp));
     }
     // return max profit
     return dp[i][buy] = profit;
@@ -63,29 +62,17 @@ int memoization(int i, int buy, vector<int> price, int n, vector<vector<int>> &d
 int tabulation(vector<int> price)
 {
     int n = price.size();
-    vector<vector<int>> dp(n + 1, vector<int>(2, 0));
-
-    // base case
-    dp[n][0] = dp[n][1] = 0;
+    vector<vector<int>> dp(n + 2, vector<int>(2, 0));
+    // base case already taken care as all values already 0
 
     // general scenario
     for (int i = n - 1; i >= 0; i--)
     {
         for (int buy = 1; buy >= 0; buy--)
         {
-            int profit = 0;
-            if (buy)
-            {
-                // we do buy or we dont buy
-                profit = max(-price[i] + dp[i + 1][0], dp[i + 1][1]);
-            }
-            // else if we can sell
-            else
-            {
-                // we do sell or we dont sell
-                profit = max(price[i] + dp[i + 1][1], dp[i + 1][0]);
-            }
-            dp[i][buy] = profit;
+
+            dp[i][1] = max(-price[i] + dp[i + 1][0], dp[i + 1][1]);
+            dp[i][0] = max(price[i] + dp[i + 2][1], dp[i + 1][0]);
         }
     }
 
@@ -97,43 +84,33 @@ int tabulation(vector<int> price)
 int spaceoptimised(vector<int> price)
 {
     int n = price.size();
-    vector<int> ahead(2, 0), curr(2, 0);
-
-    // base case
-    ahead[0] = ahead[1] = 0;
+    vector<int> ahead2(2, 0), ahead1(2,0), curr(2, 0);
+    // base case already taken care as all values already 0
 
     // general scenario
     for (int i = n - 1; i >= 0; i--)
     {
         for (int buy = 1; buy >= 0; buy--)
         {
-            int profit = 0;
-            if (buy)
-            {
-                // we do buy or we dont buy
-                profit = max(-price[i] + ahead[0], ahead[1]);
-            }
-            // else if we can sell
-            else
-            {
-                // we do sell or we dont sell
-                profit = max(price[i] + ahead[1], ahead[0]);
-            }
-            curr[buy] = profit;
+
+            curr[1] = max(-price[i] + ahead1[0], ahead1[1]);
+            curr[0] = max(price[i] + ahead2[1], ahead1[0]);
         }
-        ahead = curr;
+        ahead2 = ahead1;
+        ahead1 = curr;
     }
 
     // return scenario
     // we are returning for buy = 1 because at ind 0 we have the access to buy
-    return ahead[1];
+    return curr[1];
+
 }
 int stockbuyandsell2(vector<int> price)
 {
     int n = price.size();
-    // vector<vector<int>> dp (n+1, vector<int> (2, -1));
+    // vector<vector<int>> dp (n+2, vector<int> (2, -1));
     // return memoization(0, 1, price, n,dp);
-    return spaceoptimised(price);
+    return tabulation(price);
 }
 
 int main()
