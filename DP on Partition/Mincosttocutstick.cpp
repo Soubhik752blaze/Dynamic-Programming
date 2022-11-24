fe6f24492fa1a14f8d11ea5837d8abc62fa4e90e
Soubhik <soubhik285@gmail.com>
2022-11-25 00:48:20 +0530

    Code modified

---
+++ DP on Partition/Mincosttocutstick.cpp
@@ -0,0 +1,4 @@
+/*Question :- /
+
+// As this problem of matrix multiplication solves one big problem ( minimum operations to get A1.A2….An) and the answer varies in the order the subproblems are being solved,
+// we can identify this problem of pattern partition DP.
