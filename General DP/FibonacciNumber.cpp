44d4a05b67317ef3cc07c345356da41ca518fc5f
Soubhik <soubhik285@gmail.com>
2023-11-01 22:28:13 +0530

    code cleanup

--- General DP/FibonacciNumber.cpp
+++ General DP/FibonacciNumber.cpp
@@ -4,37 +4,37 @@ using namespace std;
 
 int fibonacci_tabulation(int n)
 {
-    //TC -> o(n), SC -> o(n)
-    //storing all computed data in the dp vector to avoid recalculations each and every time 
+    // TC -> o(n), SC -> o(n)
+    // storing all computed data in the dp vector to avoid recalculations each and every time
     vector<int> dp(n + 1, -1);
 
     dp[0] = 0;
     dp[1] = 1;
     for (int i = 2; i <= n; i++)
         dp[i] = dp[i - 1] + dp[i - 2];
-    //returning n-1th element because we are starting from 0 index so n effectively means n-1
+    // returning n-1th element because we are starting from 0 index so n effectively means n-1
     return dp[n - 1];
 }
 
 int fibonacci_optimal(int n)
 {
-    //TC -> o(n), SC -> o(1)
+    // TC -> o(n), SC -> o(1)
 
-    //same as tabulation method, the only difference being we are using variables inplace of vector
-    //this is because from logical pov, we only need the prev 2 values for calculation
-    //so for just 2 values, its better to use 2 variables and not a vector, thus saving up a lot of space 
-    int c;
-    int a = 0;
-    int b = 1;
+    // same as tabulation method, the only difference being we are using variables inplace of vector
+    // this is because from logical pov, we only need the prev 2 values for calculation
+    // so for just 2 values, its better to use 2 variables and not a vector, thus saving up a lot of space
+    int curr;
+    int prev = 0;
+    int prev2 = 1;
     for (int i = 2; i <= n; i++)
     {
-        c = a + b;
-        a = b;
-        b = c;
+        curr = prev + prev2;
+        prev2 = prev;
+        prev = curr;
     }
-    //returning n-1th element because we are starting from 0 index (and question starts from 1st index) 
-    //so n effectively means n-1
-    return a;
+    // returning n-1th element because we are starting from 0 index (and question starts from 1st index)
+    // so n effectively means n-1
+    return prev;
 }
 
 int main()
@@ -43,9 +43,9 @@ int main()
     int n = 3;
     int ans1 = fibonacci_tabulation(n);
     int ans2 = fibonacci_optimal(n);
-    cout << ans1 << " is answer from tabulation approach"<<endl;
-    cout << ans2 << " is answer from optimal approach"<< endl;
+    cout << ans1 << " is answer from tabulation approach" << endl;
+    cout << ans2 << " is answer from optimal approach" << endl;
     return 0;
 }
 
-//Most Basic Question to start DP from
+// Most Basic Question to start DP from
